8bd9bdd0145b6bf36930e3590d9891b5fc303e8e
VanerRay18 <vaneraymundo18@gmail.com>
2024-10-16 08:39:31 -0600

    Modulo Archivo

--- src/app/modules/pages/licencias/archivo-licencias/archivo-licencias.component.ts
+++ src/app/modules/pages/licencias/archivo-licencias/archivo-licencias.component.ts
@@ -1,18 +1,52 @@
-import { Component } from '@angular/core';
+import { LicenciasService } from 'src/app/services/licencias-service/licencias.service';
+import { Component, OnInit } from '@angular/core';
 import { PdfService } from 'src/app/services/pdf-service.service';
+import { ApiResponse } from 'src/app/models/ApiResponse';
+
 
 @Component({
   selector: 'app-archivo-licencias',
   templateUrl: './archivo-licencias.component.html',
   styleUrls: ['./archivo-licencias.component.css']
 })
-export class ArchivoLicenciasComponent {
+export class ArchivoLicenciasComponent implements OnInit{
   searchTerm: string = '';
   headers = ['No.', 'Nombre', 'RFC', 'FI_PS', 'Nombraminetos definitivos', 'Licencias Medicas', 'Licencias Especiales'];
-  displayedColumns = ['folio', 'desde', 'hasta', 'total_dias', 'status', 'oficio'];
-  data = [];
+  displayedColumns = ['no', 'nombre', 'rfc', ' ', ' ', 'folio',' '];
+  data: any [] = [];
 
-  constructor(private pdfService: PdfService) {}
+  constructor(
+    private pdfService: PdfService,
+    private LicenciasService :LicenciasService
+  ) {}
+
+
+  ngOnInit(): void {
+ this.fetchData();
+  }
+
+
+
+  fetchData() {
+    this.LicenciasService.getLicenciasArchivo().subscribe((response: ApiResponse) =>
+      { if (response && response.data && Array.isArray(response.data)) {
+      // Asignar el número autoincrementable a cada fila
+      this.data = response.data.map((item, index) => ({
+        ...item,
+        no: index + 1 // Añadir el número de fila autoincremental (inicia en 1)
+      }));
+      console.log(this.data); // Verificar la data con el campo 'no' agregado
+    } else {
+      console.error('La respuesta no contiene un array de datos.');
+    }
+  },
+      (error) => {
+        console.error('Error al obtener los datos:', error);
+      }
+    );
+  }
+
+
 
   generatePdfLicencias() {
     this.pdfService.generatePdfFromElement('contentToPrint', 'licencias_medicas');
