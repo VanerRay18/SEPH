638db0d7b3e9b8944e5da349d053c4309c84f1ab
Marco Vazquez <marcovazquez302@gmail.com>
2024-10-18 09:43:53 -0600

    change the API

--- src/environments/enviroment.ts
+++ src/environments/enviroment.ts
@@ -4,7 +4,8 @@ type AppEnvironment = {
     authUrl: string;
   };
   export const environment: AppEnvironment = {
-    baseService: 'http://10.10.10.208:8080',
+   // baseService: 'http://10.10.10.208:8080',
+    baseService: 'https://d932w4j4-8080.usw3.devtunnels.ms',
     logoutTime: 10680000,
     authUrl: 'https://www.google.com'
   };
