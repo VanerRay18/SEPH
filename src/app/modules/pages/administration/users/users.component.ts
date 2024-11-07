4691866e334975fe23f5415f32733de0cc8506e6
VanerRay18 <vaneraymundo18@gmail.com>
2024-11-07 11:13:50 -0600

    Utimos detalles a modulo users

--- src/app/modules/pages/administration/users/users.component.ts
+++ src/app/modules/pages/administration/users/users.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit{
   allRoles: string[] = ['Administrador', 'Licencias Admin', 'Licencias', 'Archivo Admin'];
   assignedRoles: string[] = ['Archivo'];
   allModules: string[] = ['Ingreso-licencias', 'Archivo-licenicas', 'Oficio-licenias', 'Logs-licenicas'];
@@ -13,6 +15,23 @@ export class UsersComponent {
   draggedModulo: string | null = null;
   draggedRole: string | null = null;
   showCards: boolean = false;
+  UserForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.UserForm = this.fb.group({
+      On: [true], // Checkbox de habilitado/deshabilitado
+      name: ['', [Validators.required, Validators.minLength(3)]], // Requerido, mínimo 3 caracteres
+      rfc: ['', [Validators.required, Validators.pattern(/^[A-ZÑ&]{3,4}\d{6}[A-Z0-9]{3}$/)]], // Requerido, patrón RFC
+      srl_emp: ['', [Validators.required, Validators.minLength(4)]], // Requerido, mínimo 4 caracteres
+      position: ['', Validators.required], // Requerido
+      area: ['', Validators.required], // Requerido
+      fuction: ['', [Validators.required, Validators.minLength(10)]], // Requerido, mínimo 10 caracteres
+      user: ['', [Validators.required, Validators.minLength(8)]], // Requerido, mínimo 8 caracteres
+      pass: ['', [Validators.required, Validators.minLength(6)]]  // Requerido, mínimo 6 caracteres
+    });
+  }
 
   toggleCards(activate: boolean) {
     this.showCards = activate;
